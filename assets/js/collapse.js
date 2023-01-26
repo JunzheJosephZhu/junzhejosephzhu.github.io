40875cc6d073791a87c12f8e3a52db7b99d06504
JunzheJosephZhu <junzhe.joseph.zhu@gmail.com>
2023-01-26 00:15:29 -0800

    change content layout and update js script

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -5,13 +5,15 @@ for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
+    // inline for .content, block for .content2
+    var display = content.classList.contains("content") ? "inline": "block";
     var arrow = this.children[2];
-    if (content.style.display == "inline") {
+    if (content.style.display == display) {
       content.style.display = "none";
       arrow.classList.remove("inverted");
 
     } else {
-      content.style.display = "inline";
+      content.style.display = display;
       arrow.classList.add("inverted");
     }
   });
